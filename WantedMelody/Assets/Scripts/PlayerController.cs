8f005352831c30812ba5501b7a6340c7346357eb
GregoireHENRY <GregoireHENRY@users.noreply.github.com>
2019-06-30 13:56:08 +0200

    v0.2.1
    - HUD Canvas added
    - Minimap added

--- WantedMelody/Assets/Scripts/PlayerController.cs
+++ WantedMelody/Assets/Scripts/PlayerController.cs
@@ -36,8 +36,9 @@ public class PlayerController : MonoBehaviour
 
     void JumpInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && !isJumping)
         {
+            Debug.Log("JUMP!");
             isJumping = true;
             StartCoroutine(JumpEvent());
         }
@@ -58,36 +59,4 @@ public class PlayerController : MonoBehaviour
 
         isJumping = false;
     }
-
-    /*
-    void OldController()
-    {
-        moveVertical = Input.GetAxis("Vertical");
-        moveHorizontal = Input.GetAxis("Horizontal");
-        movement = new Vector3(moveHorizontal, 0, moveVertical) * groundSpeed;
-        rb.AddRelativeForce(movement, ForceMode.VelocityChange);
-
-        // Cap the velocity to maxSpeed without reducing jumping height
-        if (rb.velocity.magnitude > maxSpeed)
-        {
-            float yVel = rb.velocity.y;
-            rb.velocity = maxSpeed * rb.velocity.normalized;
-            float xVel = rb.velocity.x;
-            float zVel = rb.velocity.z;
-            rb.velocity = new Vector3(xVel, yVel, zVel);
-        }
-
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            jumpMovement = Vector3.up * jumpSpeed;
-            rb.AddRelativeForce(jumpMovement, ForceMode.VelocityChange);
-        }
-    }
-
-    bool IsGrounded()
-    {
-        Vector3 end = new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z);
-        return Physics.CheckCapsule(col.bounds.center, end, col.radius, groundLayer);
-    }
-    */
 }
