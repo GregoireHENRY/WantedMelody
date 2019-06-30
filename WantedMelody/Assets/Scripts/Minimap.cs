4e661d94f7999c2552c2908fc5babdcbb5d08949
GregoireHENRY <GregoireHENRY@users.noreply.github.com>
2019-06-30 15:42:50 +0200

    v0.2.2
    - Terrain updated
    - Spawn point changed

--- WantedMelody/Assets/Scripts/Minimap.cs
+++ WantedMelody/Assets/Scripts/Minimap.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
     public Transform player;
+    public float yOffset;
 
     void LateUpdate()
     {
         Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
+        newPosition.y += yOffset;
         transform.position = newPosition;
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
     }
